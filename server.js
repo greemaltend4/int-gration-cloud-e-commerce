2a2b783b0e6b5bdd5dc202fb89f1daa3064c89e9
greemaltend4 <dengestabiy@hotmail.com>
2025-09-13 10:04:14 -0500

    Refactored MongoDB connection string for better readability.

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 // Connection to MongoDB
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ecommerce';
+const defaultMongoURI = 'mongodb://localhost:27017/ecommerce';
+const mongoURI = process.env.MONGODB_URI || defaultMongoURI;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Connection error to MongoDB', err));
